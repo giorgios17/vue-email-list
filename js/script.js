e188a84737c2fcf762b1445680abfe04a7bc3e81
giorgios17 <giosur@live.it>
2022-04-04 14:32:59 +0200

    done

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
 console.log('js ok')
+
+const app = new Vue({
+    el: '#app',
+    data: {
+        allEmail: [],
+    },
+    mounted() {
+        for (let i = 0; i < 10; i++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((response) => {
+                const result = response.data;
+                console.log(result);
+                this.allEmail.push(result.response)
+                console.log(this.allEmail)
+            });
+        }
+
+    }
+})
